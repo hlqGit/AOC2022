757e5866e1d752a6b4cd3680e725bfb3a724364a
hlqGit <75177981+hlqGit@users.noreply.github.com>
2022-12-06 11:56:48 -0600

    Add files via upload

---
+++ Day 4/dayFour.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class dayFour {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(new File("dayFour.dat"));
+        int total = 0;
+        while(input.hasNextLine()){
+            String[] a = new String[4];
+            a = input.nextLine().split("[-,]");
+            System.out.println(Arrays.toString(a));
+            int p1s1 = Integer.parseInt(a[0]), p1s2 = Integer.parseInt(a[1]), p2s1 = Integer.parseInt(a[2]), p2s2 = Integer.parseInt(a[3]);
+            System.out.println(p1s1 + " " + p1s2 + " " + p2s1 + " " + p2s2 + " ");
+            if(((p2s1 >= p1s1) && (p2s2 <= p1s2)) || ((p1s1 >= p2s1) && (p1s2 <= p2s2))){ total++; }
+            System.out.println(total);
+        }
+    }
+}
---
+++ Day 4/dayFour2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class dayFour2{
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(new File("dayFour.dat"));
+        int total = 0;
+        while(input.hasNextLine()){
+            boolean incased = false;
+            String[] a = new String[4];
+            a = input.nextLine().split("[-,]");
+            System.out.println(Arrays.toString(a));
+            int p1s1 = Integer.parseInt(a[0]), p1s2 = Integer.parseInt(a[1]), p2s1 = Integer.parseInt(a[2]), p2s2 = Integer.parseInt(a[3]);
+            System.out.println(p1s1 + " " + p1s2 + " " + p2s1 + " " + p2s2 + " ");
+            //if(((p2s1 >= p1s1) && (p2s2 <= p1s2)) || ((p1s1 >= p2s1) && (p1s2 <= p2s2))){ total++; incased = true; }
+            if(((p2s1 >= p1s1) && (p2s1 <= p1s2)) || ((p1s1 >= p2s1) && (p1s1 <= p2s2))) { total++; }
+            System.out.println(total);
+        }
+    }
+}
