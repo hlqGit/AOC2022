320090b1506e35b6250f89960062ed21e1b25425
hlqGit <justinjoslin0@gmail.com>
2022-12-07 15:08:42 -0600

    Finished Day 6

--- Day 6/daySix2.java
+++ Day 6/daySix2.java
@@ -5,18 +5,38 @@ import java.io.*;
 public class daySix2 {
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(new File("daySix.dat"));
-        String thing = input.nextLine(), temp = "";
+        String a = input.nextLine();
         int count = 0;
-        for(int i = 0; i < thing.length(); i ++){
-            temp = thing.substring(i, i+14);
-            for(int x = 0; x < 14; x++){
-                String check = temp.substring(x, x+1);
-                for(int z = 0; z < 14; z++){
-                    if(!(check.equals(temp.substring(z, z+1))) && x != z){
-                        System.out.println(count);
-                        break;
+        ArrayList<String> all = new ArrayList<>();
+        for(int i = 0; i < a.length(); i ++){
+            if(!(i+14 > a.length())){
+                String b = a.substring(i, i+14);
+                char[] sort = b.toCharArray();
+                System.out.println(Arrays.toString(sort));
+                Arrays.sort(sort);
+                String c = new String(sort);
+                all.add(c);
+            }
+        }
+        
+        System.out.println(all.toString());
+        for(int i = 0; i < all.size(); i++){
+            count = 0;
+            String d = all.get(i);
+            System.out.println(all.get(i));
+            for(int x = 0; x < d.length()-1; x++){
+                System.out.println(d.substring(x, x+1) + " " + d.substring(x+1, x+2));
+                if(d.substring(x, x+1).equals(d.substring(x+1,x+2))){
+                    System.out.println("fail");
+                    count = 0;
+                    break;
+                } else {
+                    count++;
+                    if (count > 12){
+                       System.out.println(i+14);
+                       return;
+                    }
                         
-                    } else { count++;}
                 }
             }
         }
